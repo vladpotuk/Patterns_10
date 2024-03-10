1588c6ac258e886037ce95b48ebba0399d748c6a
vladpotuk <vladpotuk01@gmail.com>
2024-03-10 13:16:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        
+        UserManagementFacade userFacade = new UserManagementFacade();
+
+        
+        User newUser = new User
+        {
+            Username = "Vlad",
+            Password = "pass123",
+            Email = "potukvlad@gmail.com",
+            FullName = "Potuk Vlad",
+            IsActive = true
+        };
+        userFacade.AddUser(newUser);
+
+        
+        User existingUser = userFacade.GetUser("Vlad");
+        if (existingUser != null)
+        {
+            existingUser.IsActive = false;
+            userFacade.UpdateUser(existingUser);
+        }
+
+        
+        userFacade.DeleteUser("Vlad");
+
+        Console.ReadKey();
+    }
+}
---
+++ fasade/UserManagementFacade.cs
@@ -0,0 +1,29 @@
+﻿public class UserManagementFacade
+{
+    private UserManager _userManager;
+
+    public UserManagementFacade()
+    {
+        _userManager = new UserManager();
+    }
+
+    public void AddUser(User user)
+    {
+        _userManager.AddUser(user);
+    }
+
+    public void DeleteUser(string username)
+    {
+        _userManager.DeleteUser(username);
+    }
+
+    public User GetUser(string username)
+    {
+        return _userManager.GetUser(username);
+    }
+
+    public void UpdateUser(User user)
+    {
+        _userManager.UpdateUser(user);
+    }
+}
